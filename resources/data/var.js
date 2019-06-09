9036786479536ebd954946010fbddbdf1533a312
Ashish Kumar Singh <code.aks.010@gmail.com>
2019-06-10 01:42:57 +0530

    Added Initiatives

--- resources/data/var.js
+++ resources/data/var.js
@@ -4,56 +4,155 @@ var currentPage = window.location.hash.substr(1)?window.location.hash.substr(1):
 var homeLoad = true;
 
 var initiatives = {
-	"Beladi_Salhapur": {
-		"inspire": {},
-		"scholarship": {},
-		"science_exhibition": {},
-		"self_defence_camp": {},
-		"cycle_donation": {}
-	},
-	"Chandpur": {
-		"school_adornment": {},
-		"aromatic_plant_cultivation": {},
-		"personality_dev_program": {},
-		"inf_comm_tech": {},
-		"finnancial_lit_camp": {},
-		"sanitary_pad_dist_chain": {},
-	},
-	"Charbba": {
-		"school_adornment": {},
-		"indian_state_culture": {},
-		"first_aid_dist": {},
-		"agricultural_survey": {},
-		"cycle_donation": {},
-		"inf_comm_tech": {},
-		"best_of_waste": {},
-	},
-	"Meerpur": {
-		"self_defence_camp": {},
-		"health_van": {},
-		"solving_pond_eutrophication": {},
-		"edu_emp_survey": {},
-		"inspire": {},
-		"sexual_harr_aware_camp": {},
-		"auto_polyhouse": {},
-		"campus_visit_oct18": {},
-		"inf_comm_tech": {},
-		"pond_restoration": {},
-		"pmkvy": {},
-		"cattle_vacc": {},
-		"campus_visit_march19": {},
-	},
+	"Beladi Salhapur": [
+		{
+			"title": "iNSPIRE",
+			"html": "components/initiatives/beladi/beladi1.html"
+		},
+		{
+			"title": "Scholarship",
+			"html": "components/initiatives/beladi/beladi2.html"
+		},
+		{
+			"title": "Science Exhibition",
+			"html": "components/initiatives/beladi/beladi3.html"
+		},
+		{
+			"title": "Self Defense Camp",
+			"html": "components/initiatives/beladi/beladi4.html"
+		},
+		{
+			"title": "Cycle Donation",
+			"html": "components/initiatives/beladi/beladi5.html"
+		},
+	],
+	"Chandpur": [
+		{
+			"title": "School Adornment",
+			"html": "components/initiatives/chandpur/chandpur1.html"
+		},
+		{
+			"title": "Aromatic Plant Cultivation",
+			"html": "components/initiatives/chandpur/chandpur2.html"
+		},
+		{
+			"title": "Personality Development Programme",
+			"html": "components/initiatives/chandpur/chandpur3.html"
+		},
+		{
+			"title": "Information Communication Technology",
+			"html": "components/initiatives/chandpur/chandpur4.html"
+		},
+		{
+			"title": "Finnancial Literacy Camp",
+			"html": "components/initiatives/chandpur/chandpur5.html"
+		},
+		{
+			"title": "Sanitary Pad Distribution Chain",
+			"html": "components/initiatives/chandpur/chandpur6.html"
+		},
+	],
+	"Charbba": [
+		{
+			"title": "School Adornment",
+			"html": "components/initiatives/charbba/charbba1.html"
+		},
+		{
+			"title": "Indian State Culture",
+			"html": "components/initiatives/charbba/charbba2.html"
+		},
+		{
+			"title": "First Aid Distribution",
+			"html": "components/initiatives/charbba/charbba3.html"
+		},
+		{
+			"title": "Agricultural Survey",
+			"html": "components/initiatives/charbba/charbba4.html"
+		},
+		{
+			"title": "Cycle Donation",
+			"html": "components/initiatives/charbba/charbba5.html"
+		},
+		{
+			"title": "Information Communication Technology",
+			"html": "components/initiatives/charbba/charbba6.html"
+		},
+		{
+			"title": "Best Out of Waste",
+			"html": "components/initiatives/charbba/charbba7.html"
+		},
+
+	],
+	"Meerpur": [
+		{
+			"title": "Self Defense Camp",
+			"html": "components/initiatives/meerpur/meerpur1.html"
+		},
+		{
+			"title": "Health Van",
+			"html": "components/initiatives/meerpur/meerpur2.html"
+		},
+		{
+			"title": "Solving Pond Eutrophication",
+			"html": "components/initiatives/meerpur/meerpur3.html"
+		},
+		{
+			"title": "Education Employment Survey",
+			"html": "components/initiatives/meerpur/meerpur4.html"
+		},
+		{
+			"title": "iNSPIRE",
+			"html": "components/initiatives/meerpur/meerpur5.html"
+		},
+		{
+			"title": "Sexual Harrassment Awareness Camp",
+			"html": "components/initiatives/meerpur/meerpur6.html"
+		},
+		{
+			"title": "Automatic Polyhouse",
+			"html": "components/initiatives/meerpur/meerpur7.html"
+		},
+		{
+			"title": "Information Communication Technology",
+			"html": "components/initiatives/meerpur/meerpur8.html"
+		},
+		{
+			"title": "Pond Restoration",
+			"html": "components/initiatives/meerpur/meerpur9.html"
+		},
+		{
+			"title": "PMKVY",
+			"html": "components/initiatives/meerpur/meerpur10.html"
+		},
+		{
+			"title": "Cattle Vaccination",
+			"html": "components/initiatives/meerpur/meerpur11.html"
+		},
+		{
+			"title": "Health Van",
+			"html": "components/initiatives/meerpur/meerpur12.html"
+		},
+		{
+			"title": "Campus Visit October 2018",
+			"html": "components/initiatives/meerpur/meerpur13.html"
+		},
+		{
+			"title": "Campus Visit October 2019",
+			"html": "components/initiatives/meerpur/meerpur14.html"
+		},
+	],
+	
 	"Puranpur": [{
 					"title": "Veterinary Camp",
-					"html": "components/initiatives/puranh1.html"
+					"html": "components/initiatives/puranpur/puranh1.html"
 				},
 				{
 					"title": "Health Camp",
-					"html": "components/initiatives/puranh2.html"
+					"html": "components/initiatives/puranpur/puranh2.html"
 				},
 				{
 					"title": "Adolescents Health and hygiene awareness programme",
-					"html": "components/initiatives/puranh3.html"
+					"html": "components/initiatives/puranpur/puranh3.html"
 				}],
 }
 
