212297892cca8e7e82dc49453f9c65a9f02642d6
Ashish Kumar Singh <code.aks.010@gmail.com>
2019-06-18 01:11:52 +0530

    Changes

--- resources/js/home.js
+++ resources/js/home.js
@@ -20,8 +20,8 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  // slides[slideIndex-1].style.display = "block";  
-  // dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
 
 $('.header').load("components/carousel.html", function(){
